e68942dcd0dcd4382878f988c0df166c8df4d7b4
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-22 15:20:30 +0100

    updated

--- script.js
+++ script.js
@@ -12,4 +12,37 @@ hamburger.addEventListener("click",()=>{
         hamburger.innerHTML=innerBurger
     }
 })
+
+let images = []
+images[0]="img/portrait-young-girl-with-roller-blades.jpg"
+images[1]="img/smiley-business-woman-front-view.jpg"
+images[2]= "img/jjip_jm6e_230421.jpg"
+images[3]="img/close-up-cute-child-portrait.jpg"
+images[4]="img/8666599.jpg"
+console.log(images);
+const left = document.querySelector(".fa-angle-left")
+const right = document.querySelector(".fa-angle-right")
+const img = document.getElementById("img")
+let currentIndex = 0
+function uploadImg(){
+    img.src = images[currentIndex]
+}
+right.addEventListener("click", ()=>{
+    currentIndex++
+    if(currentIndex > images.length-1){
+        currentIndex = 0
+    }uploadImg()
 })
+
+left.addEventListener("click", ()=>{
+    currentIndex--
+    if(currentIndex < 0){
+        currentIndex = images.length-1
+    }uploadImg()
+})
+
+uploadImg()
+});
+
+
+
